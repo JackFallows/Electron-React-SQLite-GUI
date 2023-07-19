5b934ab13878a33caf3a958e5a717ed6f7a73475
Jack Fallows <JackFallows@users.noreply.github.com>
2023-07-19 13:18:03 +0100

    windows version of menu

--- public/menu.cjs
+++ public/menu.cjs
@@ -3,44 +3,45 @@ const { exec_query } = require('./db.cjs');
 
 const isMac = process.platform === 'darwin';
 
+const toolsMenu = {
+    label: isMac ? app.name : 'Tools',
+    submenu: [
+        { role: 'about' },
+        { type: 'separator' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'reload' },
+        { role: 'quit' }
+    ]
+};
+
 const template = [
-    ...(isMac
-        ? [{
-            label: app.name,
-            submenu: [
-                { role: 'about' },
-                { type: 'separator' },
-                { role: 'toggledevtools' },
-                { type: 'separator' },
-                { role: 'reload' },
-                { role: 'quit' }
-            ]
-        }]
-        : []),
-        {
-            label: 'File',
-            submenu: [
-                { label: 'Open', accelerator: 'CommandOrControl+O', click: (menuItem, browserWindow, event) => {
-                    dialog.showOpenDialog({
-                        properties: ['openFile'],
-                        filters: [
-                            { name: 'Databases', extensions: ['db'] },
-                            { name: 'All files', extensions: ['*'] }
-                        ]
-                     }).then((response) => {
-                        if (response.canceled) {
-                            return;
-                        }
+    ...(isMac ? [toolsMenu] : []),
+    {
+        label: 'File',
+        submenu: [
+            { label: 'Open', accelerator: 'CommandOrControl+O', click: (menuItem, browserWindow, event) => {
+                dialog.showOpenDialog({
+                    properties: ['openFile'],
+                    filters: [
+                        { name: 'Databases', extensions: ['db'] },
+                        { name: 'All files', extensions: ['*'] }
+                    ]
+                    }).then((response) => {
+                    if (response.canceled) {
+                        return;
+                    }
 
-                        const dbPath = response.filePaths[0];
+                    const dbPath = response.filePaths[0];
 
-                        exec_query(dbPath, "SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%'").then(tables => {
-                            browserWindow.webContents.send('file-open-message', { path: dbPath, tables });
-                        });
+                    exec_query(dbPath, "SELECT name FROM sqlite_schema WHERE type='table' AND name NOT LIKE 'sqlite_%'").then(tables => {
+                        browserWindow.webContents.send('file-open-message', { path: dbPath, tables });
                     });
-                } }
-            ]
-        }
+                });
+            } }
+        ]
+    },
+    ...(isMac ? [] : [toolsMenu]),
 ];
 
 const menu = Menu.buildFromTemplate(template);
